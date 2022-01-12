2c458a5d746301c641c412d7eda7a6333d5a3bdc
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 16:24:39 +0300

    fix time diff calculation

--- src/utils/get-next-wordle-time-diff.ts
+++ src/utils/get-next-wordle-time-diff.ts
@@ -1,10 +1,12 @@
-import { WORDLE_INTERVAL_MS } from "../constants";
+import { WORDLE_INTERVAL_MS, WORDLE_START_MS } from "../constants";
 import { getCurrentWordleIndex } from "./get-current-wordle-index";
 
 export const getNextWordleTimeDiff = () => {
   const currentIndex = getCurrentWordleIndex();
-  const nextWordleTime = WORDLE_INTERVAL_MS * (currentIndex + 1);
+  const nextWordleTime =
+    WORDLE_START_MS + WORDLE_INTERVAL_MS * (currentIndex + 1);
   const now = Date.now();
+
   const diff = nextWordleTime - now;
 
   return diff;
