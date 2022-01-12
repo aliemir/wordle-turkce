24c2d2f063a034b61cbc64cf06cec1052f807e01
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 12:42:46 +0300

    handle fonts and splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { loadAsync } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { Platform, UIManager } from "react-native";
+import {
+  Merriweather_400Regular,
+  Merriweather_400Regular_Italic,
+  Merriweather_700Bold,
+  Merriweather_700Bold_Italic,
+} from "@expo-google-fonts/merriweather";
+import HomeScreen from "./src/screens/home";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const fonts = {
+  Regular: Merriweather_400Regular,
+  RegularItalic: Merriweather_400Regular_Italic,
+  Bold: Merriweather_700Bold,
+  BoldItalic: Merriweather_700Bold_Italic,
+};
+
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await loadAsync(fonts);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  React.useEffect(() => {
+    async function hide() {
+      try {
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
+    if (fontsLoaded) {
+      hide();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <HomeScreen />;
+}
