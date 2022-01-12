5b873d6c733fa6f33f0a4a5d678415c259da3029
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 14:40:46 +0300

    add guess count and length constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const WORDLE_INTERVAL_MS = 6 * 60 * 60 * 1000;
 export const WORDLE_START_MS = 1641978000000; // 2022/01/12 12:00 +03:00
 export const WORDLE_STORAGE_KEY = "@CurrentWordle";
+export const WORDLE_HISTORY_STORAGE_KEY = "@WordleHistory";
+export const MAX_GUESS_COUNT = 6;
+export const WORDLE_LENGTH = 5;
