9a1926def33cedf3d82cfe77434917997c51c1a1
Ali Emir Sen <senaliemir@gmail.com>
2022-01-13 00:20:00 +0300

    add notification status

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const WORDLE_STORAGE_KEY = "@CurrentWordle";
 export const WORDLE_HISTORY_STORAGE_KEY = "@WordleHistory";
 export const INITIAL_INSTRUCTIONS_SHOWN_STORAGE_KEY =
   "@InitialInstructionsShown";
+export const NOTIFICATIONS_STATUS_STORAGE_KEY = "@NotificationStatus";
 export const MAX_GUESS_COUNT = 6;
 export const WORDLE_LENGTH = 5;
 export const WORDLE_SHARE_MISPLACED = "🟨";
