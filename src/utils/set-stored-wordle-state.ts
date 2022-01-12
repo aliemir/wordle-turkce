c9648052cba58edda3f9f0a8626c27e854b275db
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 14:30:42 +0300

    ability to clear wordle storage

--- src/utils/set-stored-wordle-state.ts
+++ src/utils/set-stored-wordle-state.ts
@@ -2,10 +2,14 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { IWordleState } from "../@types/wordle-state";
 import { WORDLE_STORAGE_KEY } from "../constants";
 
-export const setStoredWordleState = async (state: IWordleState) => {
+export const setStoredWordleState = async (state?: IWordleState) => {
   try {
-    const stringified = JSON.stringify(state);
-    await AsyncStorage.setItem(WORDLE_STORAGE_KEY, stringified);
+    if (state) {
+      const stringified = JSON.stringify(state);
+      await AsyncStorage.setItem(WORDLE_STORAGE_KEY, stringified);
+    } else {
+      await AsyncStorage.removeItem(WORDLE_STORAGE_KEY);
+    }
   } catch (err) {
     console.warn(err);
   }
