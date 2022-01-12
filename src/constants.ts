009746aae0bc50933d2d2e1af6cea929eeb88d35
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 18:37:05 +0300

    add instructions storage key constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,8 @@ export const WORDLE_INTERVAL_MS = 6 * 60 * 60 * 1000;
 export const WORDLE_START_MS = 1641978000000; // 2022/01/12 12:00 +03:00
 export const WORDLE_STORAGE_KEY = "@CurrentWordle";
 export const WORDLE_HISTORY_STORAGE_KEY = "@WordleHistory";
+export const INITIAL_INSTRUCTIONS_SHOWN_STORAGE_KEY =
+  "@InitialInstructionsShown";
 export const MAX_GUESS_COUNT = 6;
 export const WORDLE_LENGTH = 5;
 export const WORDLE_SHARE_MISPLACED = "🟨";
