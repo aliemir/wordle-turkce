e7a29dd426db1fcb6e95d478a008eb3adb9fc687
Ali Emir Sen <senaliemir@gmail.com>
2022-01-12 16:01:35 +0300

    unite key status type

--- src/@types/wordle-state.ts
+++ src/@types/wordle-state.ts
@@ -1,5 +1,7 @@
+export type KeyStatus = "default" | "wrong" | "misplaced" | "correct";
+
 export interface IWordleState {
   wordleIndex: number;
   wordleStatus: "failed" | "completed" | "inprogress";
-  wordleRows: Array<Array<string>>;
+  wordleRows: Array<Array<{ key: string; type: KeyStatus }>>;
 }
