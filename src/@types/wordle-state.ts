eaa6e4e1de050652f5d7a69b6d3bf753f08bccc1
Ali Emir Sen <senaliemir@gmail.com>
2022-01-24 01:34:16 +0300

    add streak to state type

--- src/@types/wordle-state.ts
+++ src/@types/wordle-state.ts
@@ -2,6 +2,8 @@ export type KeyStatus = "default" | "wrong" | "misplaced" | "correct";
 
 export interface IWordleState {
   wordleIndex: number;
+  currentStreak: number;
+  bestStreak: number;
   wordleStatus: "failed" | "completed" | "inprogress";
   wordleRows: Array<Array<{ key: string; type: KeyStatus }>>;
 }
